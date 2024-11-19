b7070f74f9c2cef1444a173aa4194b5dceedd12f
Jens Kötschau <123948185+Jenseko@users.noreply.github.com>
2024-11-19 11:54:55 +0100

    fix prettier problem

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+// import tailwindPlugin from "prettier-plugin-tailwindcss";
+
+// export default {
+//   plugins: [tailwindPlugin],
+//   singleQuote: true,
+// };
+
+// module.exports = {
+//     plugins: ["prettier-plugin-tailwindcss"],
+//     singleQuote: true,
+//   };
+
+export const plugins = ["prettier-plugin-tailwindcss"];
+export const singleQuote = true;
